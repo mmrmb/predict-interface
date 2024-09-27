62a8e3b008ff0596bfac3ba30317f83e295996f7
mmrmb <mmrmb@outlook.com>
2024-09-27 17:57:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [UnoCSS(), svgr({}), react(), compression()],
   resolve: {
     alias: {
